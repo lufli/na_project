de2259405afa904aac5004f1b23c68c7c55f2195
frankbht <frankbht.ui@gmail.com>
2018-01-12 19:02:56 -0500

    modify webpack config for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   devtool: 'eval-source-map',
-  entry: `${__dirname}/client/index.js`,
+  entry: `${__dirname}/client/index.jsx`,
   output: {
     path: `${__dirname}/build`,
     filename: 'bundle.js',
   },
   module: {
     loaders: [{
-      test: /.js?$/,
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
@@ -21,6 +21,9 @@ module.exports = {
         'css-loader',
       ],
     }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     host: '127.0.0.1',
